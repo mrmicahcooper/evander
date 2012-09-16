67037382205961b63a3d536c5e186995eeea9f55
Micah Cooper <mrmicahcooper+mrmicahcooper@gmail.com>
2012-09-15 23:06:23 -0400

    Setup rails

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'hashrocket_ui', git: 'git@github.com:hashrocket/hashrocket_ui.git'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evander::Application.config.secret_token = 'c3a4868b22b3f16b461edb39057c3d0b8982888afa12bc10a0bb4bb670938924f1b30cfa4c3fbd2d189c282c9d33db5ccd12053055e8dde35f90830b2c1986d9'
